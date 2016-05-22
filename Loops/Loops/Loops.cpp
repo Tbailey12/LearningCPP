e01e97acabff2d394960c1c17b3cd9237d3d2c47
Tom Bailey <mooseinpursuit@gmail.com>
2016-05-22 17:44:02 +1000

    Loops code

---
+++ Loops/Loops/Loops.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int counter{ 0 };
+
+	//while loop
+	while (counter < 10)
+	{
+		++counter;
+		cout << "While " << counter << endl;
+	}
+
+	counter = 0;
+	//Do while loop
+	do
+	{
+		++counter;
+		cout << "Do while " << counter << endl;
+	} while (counter < 10);
+	
+	//For Loop
+	for (counter = 0; counter < 10; ++counter)
+	{
+		cout << "For " << counter+1 << endl;
+	}
+
+
+    return 0;
+}
+
